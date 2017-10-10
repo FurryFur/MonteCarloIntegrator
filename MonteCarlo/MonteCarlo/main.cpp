4b9ee5af238f52d27a2c18298eeffad6445f5fcf
FurryFur <lance.e.chaney@gmail.com>
2017-10-11 05:12:36 +1300

    Tried to fix sampling bias. Doesn't seem to be working now...

--- MonteCarlo/MonteCarlo/main.cpp
+++ MonteCarlo/MonteCarlo/main.cpp
@@ -2,27 +2,32 @@
 
 #include <matplotlibcpp.h>
 #include <glm\glm.hpp>
+#include <glm\gtc\random.hpp>
 
 #include <vector>
 #include <cmath>
 #include <random>
 #include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
 
 namespace plt = matplotlibcpp;
 using namespace glm;
+
+const dvec3 surfaceNormal = { 0, 0, 1 };
 
 //double f(double thetai, double phii, double thetar, double phir, double n)
 //{
 //	return sin(thetai) * sin(phii);
 //}
 
-double blinnPhongModified(double thetai, double phii, double thetar, double phir, double n) 
+// Blinn Phong.
+// Expects normal vectors as arguments.
+double blinnPhong(dvec3 lightDirIn, dvec3 lightDirOut, double specPow)
 {
-	return pow((cos(thetai) + cos(thetar))
-	          / sqrt(2 * (1 + sin(thetai) * cos(phii) * sin(thetar) * cos(phir)
-	                        + cos(thetai) * cos(thetar)
-	                        + sin(thetai) * sin(phii) * sin(thetar) * sin(phir))),
-	       n);
+	dvec3 halfVector = normalize(lightDirIn + lightDirOut);
+	double nDotH = dot(surfaceNormal, halfVector);
+	return pow(nDotH, specPow);
 }
 
 double fActual(double n) 
@@ -32,23 +37,20 @@ double fActual(double n)
 
 int main() 
 {
-	std::random_device rd;  //Will be used to obtain a seed for the random number engine
-	std::mt19937 randomEngine(rd()); //Standard mersenne_twister_engine seeded with rd()
+	srand(time(0));
 
 	double a = 0;
 	double b = M_PI / 2;
 	double c = 0;
 	double d = 2 * M_PI;
-	double n = 10;
-	std::uniform_real_distribution<double> THETA(a, b);
-	std::uniform_real_distribution<double> PHI(c, d);
+	double specPow = 10;
 
-	std::cout << "\nIntegrating f(x) from 0 to pi...\n\n";
+	size_t inSamples = 3000;
+	size_t reflSamples = 10000;
 
-	size_t inSamples = 1000;
-	size_t reflSamples = 50000;
-
-	std::vector<double> incomingAnglesTheta(inSamples), incomingAnglesPhi(inSamples), irradianceRatio(inSamples);
+	std::vector<double> incomingAnglesTheta(inSamples), 
+		incomingAnglesPhi(inSamples), 
+		irradianceRatio(inSamples);
 	
 	double expectedValue = 1;
 	double integralEst = 1;
@@ -57,34 +59,42 @@ int main()
 	double maxFromPhii = 0;
 	for (size_t i = 1; i <= inSamples; ++i) {
 
-		double thetai = THETA(randomEngine);
-		double phii = PHI(randomEngine);
-		
+		dvec3 lightDirIn = glm::sphericalRand(1.0); // Magnitude 1 vector on sphere
+		lightDirIn.z = std::abs(lightDirIn.z); // Reflect to get only top hemisphere
+		lightDirIn = normalize(lightDirIn);
+		lightDirIn = { 0, 0, 1 };
+
 		double sum = 0;
 		for (size_t j = 1; j <= reflSamples; ++j) {
-			double thetar = THETA(randomEngine);
-			double phir = PHI(randomEngine);
-			sum += blinnPhongModified(thetai, phii, thetar, phir, n) * cos(thetar) * sin(thetar);
+			dvec3 lightDirOut = glm::sphericalRand(1.0); // Magnitude 1 vector on sphere
+			lightDirOut.z = std::abs(lightDirOut.z); // Reflect to get only top hemisphere
+			lightDirOut = normalize(lightDirOut);
+
+			float cosThetaR = dot(surfaceNormal, lightDirOut);
+			float sinThetaR = length(cross(surfaceNormal, lightDirOut));
+			sum += blinnPhong(lightDirIn, lightDirOut, specPow) * cosThetaR * sinThetaR;
 
 			if (j == reflSamples) {
 				expectedValue = sum / j;
 				integralEst = expectedValue * (b - a) * (d - c);
 
-				incomingAnglesTheta.at(i - 1) = thetai;
-				incomingAnglesPhi.at(i - 1) = phii;
+				double thetaI = std::acos(lightDirIn.z);
+				double phiI = std::atan2(lightDirIn.y, lightDirIn.x);
+				incomingAnglesTheta.at(i - 1) = thetaI;
+				incomingAnglesPhi.at(i - 1) = phiI;
 				irradianceRatio.at(i - 1) = integralEst;
 
 				if (integralEst > maxSoFar) {
 					maxSoFar = integralEst;
-					maxFromThetai = thetai;
-					maxFromPhii = phii;
+					maxFromThetai = thetaI;
+					maxFromPhii = phiI;
 				}
 
 				printf("\nMonte Carlo E[f(x)] estimate after %zu samples is : %f\n", j, expectedValue);
-				printf("Theta incoming is : %f\n", thetai);
-				printf("phi incoming is : %f\n", phii);
+				printf("Theta incoming is : %f\n", thetaI);
+				printf("phi incoming is : %f\n", phiI);
 				printf("Monte Carlo integral(f(x)dx) estimate after %zu samples is : %f\n", j, integralEst);
-				printf("Pressumed Max : %f\n", fActual(n));
+				printf("Pressumed Max : %f\n", fActual(specPow));
 				printf("Max so far : %f, from Zenith angle : %f, Azimuth angle : %f\n", maxSoFar, maxFromThetai, maxFromPhii);
 			}
 		}
